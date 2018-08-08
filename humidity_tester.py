21f351f61ee0e640cbf61c18627d63a0e3584167
Heinrich Quirit <heinrichquirit@Heinrichs-MacBook-Pro.local>
2018-08-08 11:02:30 +1000

    Initial commit#

---
+++ humidity_tester.py
@@ -0,0 +1,13 @@
+import sense_hat from SenseHat
+sense = SenseHat()
+
+#record temperature every hour
+float humidity = sense.get_humidity()
+float temperature = sense.get_temperature()
+
+# Get datalogger
+
+
+#create database
+
+#log data
