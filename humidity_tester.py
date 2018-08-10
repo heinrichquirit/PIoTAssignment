f08a13ff83c2f373607f0ddf5f514756e97bcf79
Heinrich Quirit <heinrichquirit@gmail.com>
2018-08-10 16:37:05 +1000

    Commented steps to take

--- humidity_tester.py
+++ humidity_tester.py
@@ -4,11 +4,18 @@ sense = SenseHat()
 #record temperature every hour
 float humidity = sense.get_humidity()
 float temperature = sense.get_temperature()
-print("test")
 
-# Get datalogger
+#table structure
+#             humidity  temperature
+# timestamp
+
+#connect to database
+
+#log data in a scheduler
+    #insert into database
+
+#setup cron job to pull data from sense hat at a specific time interval
+
+#display data over a period using web interface
 
 
-#create database
-
-#log data
