f7d7bd03b1d24dbff4c8f65617474bab37547fcd
unknown <martinlfc95@gmail.com>
2018-08-19 19:50:03 +1000

    Added colour. Fixed code

--- bt.py
+++ bt.py
@@ -9,8 +9,7 @@ def raspberryDevices(pi):
   else:
     print("No devices named pi")
 
-
-#search for nearby bluetooth devices and print 1st 5 nearby devices
+#search for nearby bluetooth devices
 def search():
   nearby_devices = bluetooth.discover_devices()
   print("Here are all the nearby devices")
@@ -18,17 +17,15 @@ def search():
     devices = str(bluetooth.lookup_name(x)) + " [" + str(x) + "]"
     device_name = (bluetooth.lookup_name(x))
     print(devices)
-    
-    if ("MARTIN")  or  ("Martin") in device_name:
-      print("Hello {}".format(device_name))
-      print("---------------------------------")
-      sense = SenseHat()
-      sense.show_message("Hello {}".format(device_name), scroll_speed=0.05)
+
+  if ("MARTIN")  or  ("Martin") in device_name:
+    print ("")
+    sense = SenseHat()
+    temp = round(sense.get_temperature(), 1)
+    sense.show_message("Hello {}! The Temperature is {}".format(device_name, temp), scroll_speed=0.05, text_colour=(0, 0, 255))
 
   print("Here are all the Raspberry Pi Devices")
   print(raspberryDevices(devices))
-
-
 
 #main function
 def main():
