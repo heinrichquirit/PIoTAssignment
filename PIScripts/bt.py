0bcd4775ebf3aa1251b2aaf4d5235c820d95458b
unknown <martinlfc95@gmail.com>
2018-08-19 21:20:02 +1000

    Final Push

--- PIScripts/bt.py
+++ PIScripts/bt.py
@@ -2,14 +2,14 @@ import bluetooth
 import os
 from sense_hat import SenseHat
 
-#find the string "Pi" in device
+#find the string "Raspberry" in device
 def raspberryDevices(pi):
-  if ("Raspberry Pi") or ("Pi") in pi:
+  if "Raspberry Pi" in pi:
     print(pi)
   else:
     print("No Raspberry Pi devices nearby")
 
-#search for nearby bluetooth devices
+#search for nearby bluetooth devices. Print them out and greet all raspberry pi devices on the Sense Hat
 def search():
   nearby_devices = bluetooth.discover_devices()
   print("Here are all the nearby devices")
@@ -17,17 +17,17 @@ def search():
     devices = str(bluetooth.lookup_name(x)) + " [" + str(x) + "]"
     device_name = (bluetooth.lookup_name(x))
     print(devices)
+  
+    print ("")  
+    print("Here are all the Raspberry Pi Devices")
+    print(raspberryDevices(devices))
 
-  if ("Raspberry Pi") or ("Pi") in device_name:
-    print ("")
+  if "Raspberry Pi" in device_name:
     sense = SenseHat()
     temp = float(sense.get_temperature())
     cpu_temp = float(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
     calibrated_temp = round(cpu_temp - temp, 2)
     sense.show_message("Hello {}! The Temperature is {}".format(device_name, calibrated_temp), scroll_speed=0.05, text_colour=(0, 0, 255))
-
-  print("Here are all the Raspberry Pi Devices")
-  print(raspberryDevices(devices))
 
 #main function
 def bt_main():
